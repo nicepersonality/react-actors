0464d70ff9cce0a2d691cdd02d13437266d9cce9
Lilith Bentley <sbentley@gmail.com>
2019-09-17 16:44:52 -0500

    Divide App into components.

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="App-contact">
+        <h2>Contact</h2>
+        <p>Phone</p>
+        <p>Email</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="App-footer">
+        <p>&copy;2019 React Actors</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
